d5876ffa1d7dfff10d3dd02cf5ffe485dbbe605e
Kenneth Hammond <khammond@pppl.gov>
2025-02-26 22:05:30 +0100

    Added test with steps copied from regularized_constrained_least_squares

--- tests/github_debug/test_wf_factorization.py
+++ tests/github_debug/test_wf_factorization.py
@@ -108,15 +108,70 @@ class WireframeFactorizationTests(unittest.TestCase):
         print('        R.T[:,:6] = ')
         print_matrix(R3.T[:,:6])
 
-        print('Calling regularized_constrained_least_squares')
+        print('Performing the exact sequence of steps in regularized_constrained_least_squares up to QR')
         A4, b4 = bnorm_obj_matrices(wf, surf_plas)
         C4, d4 = wf.constraint_matrices(assume_no_crossings=False,
                                         remove_constrained_segments=True)
-        x4 = regularized_constrained_least_squares(A4, b4, reg_W, C4, d4)
+
+        # Recast inputs as Numpy arrays
+        Amat = np.array(A4)
+        bvec = np.array(b4).reshape((-1,1))
+        Ctra = np.array(C4).T # Transpose will be used for the calculations
+        dvec = np.array(d4).reshape((-1,1))
+    
+        # Check the inputs
+        m, n = Amat.shape
+        if bvec.shape[0] != m:
+            raise ValueError('Number of elements in b must match rows in A')
+        n_C, p = Ctra.shape
+        if n_C != n:
+            raise ValueError('A and C must have the same number of columns')
+        if dvec.shape[0] != p:
+            raise ValueError('Number of elements in d must match rows in C')
+    
+        if np.isscalar(reg_W):
+            Wmat = reg_W*np.eye(n)
+        else:
+            Wmat = np.squeeze(reg_W)
+            if len(Wmat.shape) == 1:
+                if Wmat.shape[0] != n:
+                    raise ValueError('Number of elements in vector-form W ' \
+                                     'must match columns in A')
+                Wmat = np.diag(Wmat)
+            elif len(Wmat.shape) == 2:
+                if Wmat.shape[0] != n or Wmat.shape[1] != n:
+                    raise ValueError('Number of rows and columns in matrix-form W '\
+                                     'must both equal number of columns in A')
+            else:
+                raise ValueError('W must be a scalar, 1d array, or 2d array')
+                
+        # Compute the QR factorization of the transpose of the constraint matrix
+        Qfull, Rtall = scipy.linalg.qr(Ctra)
+        Q1mat = Qfull[:,:p]  # Orthonormal vectors in the constrained subspace
+        Q2mat = Qfull[:,p:]  # Orthonormal vectors in the free subspace
+        Rmat = Rtall[:p,:]
+
+        print('    Transpose of constraint matrix:')
+        print_matrix(Ctra)
+        print('    R from QR-factorization:')
+        if np.all(np.isfinite(Rmat)):
+            print('        Contains no infinite/NaN elements!')
+        else:
+            print('        Contains some infinite/NaN elements!')
+        print('        R.T[:,:6] = ')
+        print_matrix(Rmat.T[:,:6])
+
+
+
+        print('Calling regularized_constrained_least_squares')
+        A5, b5 = bnorm_obj_matrices(wf, surf_plas)
+        C5, d5 = wf.constraint_matrices(assume_no_crossings=False,
+                                        remove_constrained_segments=True)
+        x5 = regularized_constrained_least_squares(A5, b5, reg_W, C5, d5)
 
         print('Calling rcls_wireframe')
-        A5, b5 = bnorm_obj_matrices(wf, surf_plas)
-        x5, f_B5, f_R5, f5 = rcls_wireframe(wf, A5, b5, reg_W, False, True)
+        A6, b6 = bnorm_obj_matrices(wf, surf_plas)
+        x6, f_B6, f_R6, f6 = rcls_wireframe(wf, A6, b6, reg_W, False, True)
 
         print('Calling optimize_wireframe')
         res = optimize_wireframe(wf, 'rcls', {'reg_W': reg_W}, 
